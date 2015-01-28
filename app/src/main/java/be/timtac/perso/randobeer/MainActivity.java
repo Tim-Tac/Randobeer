fe26d51b0d0e2eecd8763da774b4cb60704bd8cf
Tim Dep <titidep001@hotmail.com>
2015-01-28 22:01:53 +0100

    nouvelle icône + suite des listes dynamiques

--- app/src/main/java/be/timtac/perso/randobeer/MainActivity.java
+++ app/src/main/java/be/timtac/perso/randobeer/MainActivity.java
@@ -5,15 +5,22 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
-    private String[] beers = {};
-    private String[] persons = {};
+    private List<String> bieres = new ArrayList<String>();
+    private List<String> personnes = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +31,31 @@ public class MainActivity extends Activity {
         final Button lancer = (Button)findViewById(R.id.lancer);
         final Button beer = (Button)findViewById(R.id.beer);
         final Button person = (Button)findViewById(R.id.person);
+        final LinearLayout monlayout= (LinearLayout)findViewById(R.id.layout);
+
+
+        TextView texte[] = new TextView[bieres.size()];
+        int i=0;
+        for(i=0;i<bieres.size();i++)
+        {
+            texte[i] = new TextView(this);
+            texte[i].setTextSize(14);
+            texte[i].setText("hi");
+
+            RelativeLayout.LayoutParams textViewLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            textViewLayoutParams.setMargins(5, 30, 0, 0);
+            texte[i].setLayoutParams(textViewLayoutParams);
+            texte[i].setGravity(Gravity.RIGHT);
+            monlayout.addView(texte[i]);
+           Toast.makeText(getApplicationContext(),"passé",Toast.LENGTH_SHORT).show();
+        }
 
 
     /**********************************************************************************************/
 
     lancer.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
-           if((beers.length == 0) || (persons.length == 0))
+           if((bieres.isEmpty()) || (personnes.isEmpty()))
            {
                Toast.makeText(getApplicationContext(),"Au moins une liste vide",Toast.LENGTH_SHORT).show();
            }
@@ -51,7 +76,7 @@ public class MainActivity extends Activity {
 
               final EditText input = new EditText(MainActivity.this);
               input.setHint("entrez votre bière");
-              input.setPadding(15,60,0,15);
+              input.setPadding(20,60,0,20);
               input.setTextSize(20);
               builder.setView(input);
 
@@ -59,23 +84,20 @@ public class MainActivity extends Activity {
               builder.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
                   @Override
                   public void onClick(DialogInterface dialog, int which) {
-                      String test = input.getText().toString();
-                      Toast.makeText(getApplicationContext(),test,Toast.LENGTH_SHORT).show();
+                      String tbiere = input.getText().toString();
                   }
               });
 
               builder.setNegativeButton("Annuler",new DialogInterface.OnClickListener() {
                   @Override
                   public void onClick(DialogInterface dialog, int which) {
-                      Toast.makeText(getApplicationContext(),"vous avez annulé",Toast.LENGTH_SHORT).show();
                   }
               });
 
             AlertDialog dialog = builder.create();
-              dialog.show();
+            dialog.show();
 
-
-
+            bieres.add("tbiere");
           }
       });
 
@@ -83,10 +105,37 @@ public class MainActivity extends Activity {
     person.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Ajout de personne");
+
+            final EditText input = new EditText(MainActivity.this);
+            input.setHint("entrez un nom");
+            input.setPadding(20,60,0,20);
+            input.setTextSize(20);
+            builder.setView(input);
+
+
+            builder.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String tpers = input.getText().toString();
+                }
+            });
+
+            builder.setNegativeButton("Annuler",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            personnes.add("tpers");
+
         }
     });
-
-
 
     }
 }
