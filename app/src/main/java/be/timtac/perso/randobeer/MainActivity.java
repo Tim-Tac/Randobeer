6dd0b7a8179a6927663ea6eb98a41fc27c5fe39e
Tim Dep <titidep001@hotmail.com>
2015-08-22 14:11:05 +0200

    Basic UI

--- app/src/main/java/be/timtac/perso/randobeer/MainActivity.java
+++ app/src/main/java/be/timtac/perso/randobeer/MainActivity.java
@@ -3,17 +3,26 @@ package be.timtac.perso.randobeer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
 
-    private List<String> beer = new ArrayList<String>();
-    private List<String> people = new ArrayList<String>();
+    private List<String> beer = new ArrayList<>();
+    private List<String> people = new ArrayList<>();
     private String tpers;
 
     //UI declaration
-    private Button lancer = null;
+    private Button go;
+    private Button addSomeOne;
+    private Button addBeverage;
+    private ListView listBeverage;
+    private ListView listSomeone;
+    private TextView nobody;
+    private TextView noBeverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +30,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lancer = (Button) findViewById(R.id.launch);
+        go = (Button) findViewById(R.id.launch);
     }
 }
